ce2448eb0ee64a9b39145500ae23f525dbc28a5f
thiagoboeker <thiago2kboeker@gmail.com>
2019-09-21 16:31:39 -0300

    Admin auth adicionado

---
+++ lib/tecnovix_web/auth/admin.ex
@@ -0,0 +1,42 @@
+defmodule TecnovixWeb.Auth.Commom do
+  @moduledoc """
+  Modulo responsavel por autenticacao interna.
+
+  ## Usage
+
+    `
+    pipeline "api" do
+      plug :commom_auth
+    end
+    `
+  """
+
+  use Plug.Builder
+
+  @salt System.fetch_env!("SALT")
+  @exp 3_600
+
+  @doc false
+  def get_token(conn = %Plug.Conn{}) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization") do
+      {:ok, token}
+    else
+      _ -> {:error, :invalid}
+    end
+  end
+
+  @doc """
+  Autenticacao interna. Adicionar o caso de `{:error, :invalid}` no fallback para evitar possiveis erros
+  com status 500. 
+  """
+  def commom_auth(conn = %Plug.Conn{}, _opts) do
+    with {:ok, token} <- get_token(conn),
+         {:ok, user} <- Phoenix.Token.verify(TecnovixWeb.Endpoint, @salt, token, max_age: @exp) do 
+      put_private(conn, :auth, user)
+    else
+      error ->
+        halt(conn)
+        error
+    end
+  end
+end
--- lib/tecnovix_web/auth/firebase.ex
+++ lib/tecnovix_web/auth/firebase.ex
@@ -2,13 +2,15 @@ defmodule TecnovixWeb.Auth.Firebase do
   @moduledoc """
   Autentica o usuario do Firebase. Esta no formato de Plug
 
-  `
-  import TecnovixWeb.Auth.Firebase
-  #...
-  pipeline "api" do
-    plug :firebase_auth
-  end
-  `
+  ## Usage
+
+    `
+    import TecnovixWeb.Auth.Firebase
+    #...
+    pipeline "api" do
+      plug :firebase_auth
+    end
+    `
   """
 
   use Plug.Builder
@@ -22,16 +24,22 @@ defmodule TecnovixWeb.Auth.Firebase do
   """
   def get_token(conn = %Plug.Conn{}) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization") do
-      {:ok, token}  
+      {:ok, token}
     else
       _ -> {:error, :not_authorized}
     end
   end
-  
+
+  @doc """
+  Retira a parte protegida da token em forma de %JOSE.JWS{}.
+  """
   def peek(token) when is_binary(token) do
     JOSE.JWT.peek_protected(token)
   end
 
+  @doc """
+  Identifica a public key usada para gerar a IdToken passada.
+  """
   def get_public_key(payload = %JOSE.JWS{}) do
     @public_key
     |> Map.keys()
@@ -39,15 +47,23 @@ defmodule TecnovixWeb.Auth.Firebase do
     |> Enum.fetch(0)
   end
 
+  @doc """
+  Após a identificacao da chave publica, retorna a JWK referente para verificar a token
+  """
   def get_jwk({:ok, key}) do
-      @public_key
-      |> Map.get(key)
+    @public_key
+    |> Map.get(key)
   end
 
   def verify_jwt(jwk, token) do
     JOSE.JWT.verify(jwk, token)
   end
 
+  @doc """
+  Verifica a JWT passada na requisiçao.
+
+  Retorna `{true, %JOSE.JWT{}, %JOSE.JWS{}}` para tokens validas  
+  """
   def verify_jwt({:init, token}) do
     token
     |> peek()
@@ -56,13 +72,20 @@ defmodule TecnovixWeb.Auth.Firebase do
     |> verify_jwt(token)
   end
 
+  @doc """
+  Plug para autenticacao com o firebase. Sempre lembrar de adicionar o caso de `{:error, :not_authorized}`
+  no Fallback do router.
+  Caso contrario para o resto do lifecycle da requisiçao, os dados do JWT estaram na key `:auth` do campo
+  private da `%Plug.Conn{}`.
+  """
   def firebase_auth(conn = %Plug.Conn{}, _opts) do
     with {:ok, token} <- get_token(conn),
          {true, jwt = %JOSE.JWT{}, _jws} <- verify_jwt({:init, token}) do
-      conn
-      |> put_private(:auth, jwt)
+      put_private(conn, :auth, jwt)
     else
-      _ -> {:error, :not_authorized}
+      _ ->
+        halt(conn)
+        {:error, :not_authorized}
     end
   end
 end
