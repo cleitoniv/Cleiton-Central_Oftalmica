3a7c371994d6b7602ad688199a62270b7c4e2b50
Victoraugusto77 <victorasilva0707@gmail.com>
2020-07-08 21:32:34 -0300

    Recurso para mostrar os dados do cliente/usuario ao logar

--- lib/tecnovix_web/controllers/clientes_controller.ex
+++ lib/tecnovix_web/controllers/clientes_controller.ex
@@ -3,6 +3,8 @@ defmodule TecnovixWeb.ClientesController do
   use Tecnovix.Resource.Routes, model: Tecnovix.ClientesModel
   alias Tecnovix.ClientesModel
   alias Tecnovix.UsuariosClienteModel
+  alias Tecnovix.AtendPrefClienteModel
+
   action_fallback Tecnovix.Resources.Fallback
 
   def create_user(conn, %{"param" => params}) do
@@ -16,5 +18,33 @@ defmodule TecnovixWeb.ClientesController do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(%{sucess: true}))
+  end
+
+  def show(conn, _params) do
+    case conn.private.auth do
+      {:ok, %Tecnovix.ClientesSchema{} = cliente} ->
+        {:ok, map} = ClientesModel.show(cliente.id)
+        user = Map.put(map, :atend_pref_cliente, AtendPrefClienteModel.get_by(cliente_id: cliente.id))
+
+        conn
+        |> put_status(:ok)
+        |> put_resp_content_type("applicaton/json")
+        |> put_view(TecnovixWeb.ClientesView)
+        |> render("show_cliente.json", %{item: user})
+
+      {:ok, %Tecnovix.UsuariosClienteSchema{} = usuario} ->
+        {:ok, map} = UsuariosClienteModel.show(usuario.id)
+        user =
+        Map.put(map, :cliente, ClientesModel.get_by(id: usuario.cliente_id))
+        |> Map.put(:atend_pref_cliente, AtendPrefClienteModel.get_by(cliente_id: usuario.cliente_id))
+
+        conn
+        |> put_status(:ok)
+        |> put_resp_content_type("applicaton/json")
+        |> put_view(TecnovixWeb.ClientesView)
+        |> render("show_usuario.json", %{item: user})
+
+        _ -> {:error, :invalid_parameter}
+    end
   end
 end
