1262b621eb20172f63dc2dc6391277041185e938
Reginaldo Gutter <reginaldogutter@gmail.com>
2020-07-29 13:04:30 -0300

    merged 5119-MODULO_DE_CLIENTE/USUARIO-LOGADO

--- lib/tecnovix/model/atend_pref_cliente.ex
+++ lib/tecnovix/model/atend_pref_cliente.ex
@@ -7,4 +7,14 @@ defmodule Tecnovix.AtendPrefClienteModel do
       atend_pref -> __MODULE__.update(atend_pref, params)
     end
   end
+
+  def insert_or_update(params) do
+    with nil <- Repo.get_by(AtendPrefClienteSchema, cod_cliente: params["cod_cliente"]),
+         nil <- Repo.get_by(AtendPrefClienteSchema, loja_cliente: params["loja_cliente"]) do
+          create(params)
+    else
+      cliente ->
+        {:ok, cliente}
+    end
+  end
 end
