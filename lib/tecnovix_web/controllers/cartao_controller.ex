23f0f8a6b83489ac8ff75ba542324eda5acd14db
Victoraugusto77 <victorasilva0707@gmail.com>
2020-08-28 16:28:19 -0300

    create card

--- lib/tecnovix_web/controllers/cartao_controller.ex
+++ lib/tecnovix_web/controllers/cartao_controller.ex
@@ -1,8 +1,18 @@
-defmodule TecnovixWeb.CartaoController do
+defmodule TecnovixWeb.CartaoCreditoClienteController do
   use TecnovixWeb, :controller
   use Tecnovix.Resource.Routes, model: Tecnovix.CartaoDeCreditoModel
   alias Tecnovix.CartaoDeCreditoModel, as: CartaoModel
-  alias Tecnovix.Resource.Wirecard.Actions, as: Wirecard
 
   action_fallback Tecnovix.Resources.Fallback
+
+  def create(conn, %{"param" => params}) do
+    with {:ok, card} <- CartaoModel.create(params) do
+      conn
+      |> put_status(200)
+      |> put_resp_content_type("application/json")
+      |> render("show.json", %{item: card})
+    else
+      _ -> {:error, :not_created}
+    end
+  end
 end
