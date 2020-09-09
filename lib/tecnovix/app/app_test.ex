b9d926fb6a00e4843d6a847ac3245ebb61a9731a
unknown <victorasilva0707@gmail.com>
2020-09-09 17:28:35 -0300

    fix

--- lib/tecnovix/app/app_test.ex
+++ lib/tecnovix/app/app_test.ex
@@ -333,6 +333,42 @@ defmodule Tecnovix.App.ScreensTest do
           end
         end)
     end)
+    |> Enum.map(
+    fn paciente ->
+      group_by = Enum.group_by(paciente.items, fn item -> item.codigo_item end)
+      |> Enum.map(fn {codigo, codigo_items} ->
+        Enum.reduce(codigo_items, %{}, fn codigo_item, acc ->
+          p_olho = parse_olho(codigo_item)
+
+          map =
+            Map.put(acc, :valor_produto, codigo_item.valor_produto)
+            |> Map.put(:quantidade, codigo_item.quantidade)
+            |> Map.put(:valor_total, codigo_item.valor_total)
+            |> Map.put(:olho, codigo_item.olho)
+            |> Map.put(:url_image, @product_url)
+            |> Map.put(:codigo_item, codigo_item.codigo_item)
+            |> Map.put(:duracao, "1 Ano")
+
+          Map.merge(map, p_olho)
+        end)
+       end)
+       Map.put(paciente, :items, group_by)
+    end)
+  end
+
+  defp parse_olho(item) do
+    case item.olho do
+      "D" -> %{
+        esferico_d: item.esferico,
+        eixo_d: item.eixo,
+        cilindro_d: item.cilindro
+      }
+      "E" -> %{
+        esferico_e: item.esferico,
+        eixo_e: item.eixo,
+        cilindro_e: item.cilindro
+      }
+    end
   end
 
   def get_pedido_id(cliente_id, pedido_id) do
