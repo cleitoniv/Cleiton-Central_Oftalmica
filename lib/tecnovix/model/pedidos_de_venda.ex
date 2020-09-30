225dde2b52e910dcb3fca5aae1d76ccf0204f3db
unknown <victorasilva0707@gmail.com>
2020-09-30 15:27:57 -0300

    fix

--- lib/tecnovix/model/pedidos_de_venda.ex
+++ lib/tecnovix/model/pedidos_de_venda.ex
@@ -85,7 +85,8 @@ defmodule Tecnovix.PedidosDeVendaModel do
       |> PedidosDeVendaModel.payment_params()
       |> PedidosDeVendaModel.wirecard_payment()
       |> Wirecard.create_payment(order_id)
-
+      |> IO.inspect
+      
     case payment do
       {:ok, %{status_code: 201}} -> payment
       _ -> {:error, :payment_not_created}
