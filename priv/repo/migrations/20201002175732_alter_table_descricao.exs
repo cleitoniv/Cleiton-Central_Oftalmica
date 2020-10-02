034a737cbb83c2acb938c00efe211043f95f1c84
unknown <victorasilva0707@gmail.com>
2020-10-02 15:09:34 -0300

    heroku

--- priv/repo/migrations/20201002175732_alter_table_descricao.exs
+++ priv/repo/migrations/20201002175732_alter_table_descricao.exs
@@ -2,7 +2,9 @@ defmodule Tecnovix.Repo.Migrations.AlterTableDescricao do
   use Ecto.Migration
 
   def change do
-    remove :codigo
-    add :codigo, :string
+    alter table(:descricao_generica_do_produto) do
+      remove :codigo
+      add :codigo, :string
+    end
   end
 end
