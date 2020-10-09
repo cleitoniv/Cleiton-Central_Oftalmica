c610157b636826fd402c2dc110c05d6ed1d6d2c8
unknown <victorasilva0707@gmail.com>
2020-10-09 12:51:36 -0300

    heroku

--- lib/tecnovix/app/app_prod.ex
+++ lib/tecnovix/app/app_prod.ex
@@ -699,7 +699,7 @@ defmodule Tecnovix.App.ScreensProd do
       end)
 
     product =
-      Enum.map(product, fn map ->
+      Enum.flat_map(product, fn map ->
         Map.put(map, "image_url", "http://portal.centraloftalmica.com/images/#{map["group"]}.jpg")
       end)
 
