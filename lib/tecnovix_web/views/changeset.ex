ba31a879d3648f4285f9a8ce08e7224adae7daa5
unknown <victorasilva0707@gmail.com>
2020-10-15 13:05:17 -0300

    heroku

--- lib/tecnovix_web/views/changeset.ex
+++ lib/tecnovix_web/views/changeset.ex
@@ -6,7 +6,14 @@ defmodule TecnovixWeb.ChangesetView do
   end
 
   def render("error.json", %{changeset: changeset}) do
-    %{success: false, data: %{errors: translate_errors(changeset)}}
+    error =
+      translate_errors(changeset)
+      |> Enum.map(fn {key, value} ->
+        {String.upcase(Atom.to_string(key)), value}
+      end)
+      |> Map.new()
+
+    %{success: false, data: %{errors: error}}
   end
 
   def render("multi_error.json", %{changeset: changeset, identifiers: identifiers}) do
