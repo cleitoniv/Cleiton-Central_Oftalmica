56b33850701da1fa4e63679a2fff75a39e58bd8c
unknown <victorasilva0707@gmail.com>
2020-10-27 13:37:41 -0300

    heroku

--- lib/tecnovix_web/views/pedidos_de_venda.ex
+++ lib/tecnovix_web/views/pedidos_de_venda.ex
@@ -19,6 +19,7 @@ defmodule TecnovixWeb.PedidosDeVendaView do
         pd_correios: item.pd_correios,
         vendedor_1: item.vendedor_1,
         status_ped: item.status_ped,
+        pago: item.pago,
         inserted_at: item.inserted_at,
         updated_at: item.updated_at,
         items:
