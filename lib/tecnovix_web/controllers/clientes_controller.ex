2c8beab4c12f88590f25da481b6fb6e1b585eda5
unknown <victorasilva0707@gmail.com>
2020-11-19 17:29:11 -0300

    heroku

--- lib/tecnovix_web/controllers/clientes_controller.ex
+++ lib/tecnovix_web/controllers/clientes_controller.ex
@@ -412,6 +412,8 @@ defmodule TecnovixWeb.ClientesController do
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(200, Jason.encode!(%{"success" => true, "data" => "Inserido."}))
+    else
+      v -> IO.inspect v
     end
   end
 
