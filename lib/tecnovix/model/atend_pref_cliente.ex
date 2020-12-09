d59f6eeb80d55956b485e3bb1c2b0abf246437da
unknown <victorasilva0707@gmail.com>
2020-12-09 15:21:49 -0300

    heroku

--- lib/tecnovix/model/atend_pref_cliente.ex
+++ lib/tecnovix/model/atend_pref_cliente.ex
@@ -71,7 +71,7 @@ defmodule Tecnovix.AtendPrefClienteModel do
           changeset ->
             previous =
               Enum.flat_map(Map.from_struct(changeset), fn {key, value} ->
-                case value == 1 and key != :id and key == horario_new do
+                case value == 1 and key != :id and key != horario_new do
                   true -> [key]
                   false -> []
                 end
@@ -103,7 +103,8 @@ defmodule Tecnovix.AtendPrefClienteModel do
           changeset ->
             previous =
               Enum.flat_map(Map.from_struct(changeset), fn {key, value} ->
-                case value == 1 and key != :id and key == horario_new do
+                IO.inspect key
+                case value == 1 and key != :id and key != horario_new do
                   true -> [key]
                   false -> []
                 end
