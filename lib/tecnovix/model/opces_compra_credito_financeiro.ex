21ad7ed962cd9713ffe2d3f8a34f1ecc931e7d88
unknown <victorasilva0707@gmail.com>
2020-12-09 16:06:25 -0300

    heroku

--- lib/tecnovix/model/opces_compra_credito_financeiro.ex
+++ lib/tecnovix/model/opces_compra_credito_financeiro.ex
@@ -5,5 +5,10 @@ defmodule Tecnovix.OpcoesCompraCreditoFinanceiroModel do
 
   def get_offers() do
     Repo.all(OpcoesCredito)
+    |> Enum.map(fn credito ->
+      desconto = credito.valor * (credito.desconto / 100)
+
+      credito.valor - desconto
+    end)
   end
 end
