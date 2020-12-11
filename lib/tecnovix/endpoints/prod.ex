513769321e05ef383166ba0c1aba722eb417644f
unknown <victorasilva0707@gmail.com>
2020-12-11 17:10:34 -0300

    heroku

--- lib/tecnovix/endpoints/prod.ex
+++ lib/tecnovix/endpoints/prod.ex
@@ -148,6 +148,8 @@ defmodule Tecnovix.Endpoints.ProtheusProd do
     end
    end
 
+   IO.inspect field
+
     case field["id"] do
       "A1_DTNASC" ->
         ano = String.slice(check(field["value"]), 0..3)
