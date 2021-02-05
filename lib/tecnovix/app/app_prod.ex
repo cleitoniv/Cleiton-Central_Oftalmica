44d82368c638f2fd32ce4171cf969ab3989bbf15
Thiago Boeker <thiago2kboeker@gmail.com>
2021-02-05 15:34:18 -0300

    heroku

--- lib/tecnovix/app/app_prod.ex
+++ lib/tecnovix/app/app_prod.ex
@@ -263,7 +263,7 @@ defmodule Tecnovix.App.ScreensProd do
           quantidades -> [Map.put(produto, "boxes", produto["boxes"] - Enum.sum(quantidades))] ++ acc
         end
       end)
-      |> Enum.sort(:desc)
+      |> Enum.sort(:asc)
 
     filters = organize_filters_grid(produtos)
 
