c235e1c8f778a613a8e5bea9b76305ce077bf728
thiagoboeker <thiago2kboeker@gmail.com>
2022-02-17 13:49:49 -0300

    feat: password reset added

--- lib/tecnovix_web/controllers/usuarios_cliente_controller.ex
+++ lib/tecnovix_web/controllers/usuarios_cliente_controller.ex
@@ -48,7 +48,7 @@ defmodule TecnovixWeb.UsuariosClienteController do
 
       {:ok, %{status_code: 400}} ->
         with {:ok, user} <- UsuariosClienteModel.create(params |> Map.put("uid", body_decode["localId"])),
-             _ -> Firebase.reset_password(%{email: params["email"]}) do
+             _ <- Firebase.reset_password(%{email: params["email"]}) do
          conn
          |> put_status(:created)
          |> put_resp_content_type("application/json")
